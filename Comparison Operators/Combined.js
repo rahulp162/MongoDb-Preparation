354226480e932540f89278ab7dc3a1ca922980fa
Rahul Panchal <Rahul.Panchal@upforce.tech>
2025-01-28 17:28:38 +0530

    forgot to remove .count()s

--- Comparison Operators/Combined.js
+++ Comparison Operators/Combined.js
@@ -334,21 +334,21 @@ db.products.find({
   tags:{
       $nin:["smartphone", "headphones"]
   }
-}, {productName:1, _id:0, tags:1}).count()
+}, {productName:1, _id:0, tags:1}) 
 
 //Question-20: Find products with exactly 2 related products.
 db.products.find({
 relatedProducts:{
   $size:2
 }
-},{productName:1, relatedProducts:1}).count()
+},{productName:1, relatedProducts:1}) 
 
 //Question-21: Find products with ratings where at least one score is greater than or equal to 5.
 db.products.find({
 "ratings.score":{
   $gte:5
 }
-},{productName:1,_id:0,"ratings.score":1}).count()
+},{productName:1,_id:0,"ratings.score":1}) 
 
 //Question-22: Find products with a price between $500 and $1000, inclusive, and sort them by price in descending order.
 db.products.find({
@@ -356,14 +356,14 @@ price: {
   $gte: 500,
   $lte: 1000
 }
-},{productName:1, price:1, _id:0}).sort({price:-1}).count()
+},{productName:1, price:1, _id:0}).sort({price:-1}) 
 
 //question-23: Find products released after June 2022 and skip the first 2 results.
 db.products.find({
 releaseDate:{$gt:ISODate("2022-06-01")}
 },{
 productName:1, releaseDate:1, _id:0
-}).skip(2).count()
+}).skip(2) 
 
 //Question-24: Find all products that are either "isFeatured" or "isBestSeller" and limit the results to 3 documents.
 db.products.find({
@@ -371,7 +371,7 @@ $or:[
   {isFeatured:true},
   {isBestSeller:true}
 ]
-},{productName:1, isBestSeller:1, isFeatured:1, _id:0}).limit(3).count()
+},{productName:1, isBestSeller:1, isFeatured:1, _id:0}).limit(3) 
 
 //Question-25: Find products with a warranty of either "3 years" or "5 years".
 db.products.find({
@@ -379,28 +379,28 @@ db.products.find({
     {warranty: "2 years"},  
     {warranty: "5 years"},  
   ] 
-},{productName:1, warranty:1, _id:0}).count()
+},{productName:1, warranty:1, _id:0}) 
 
 //Question-26: Find products that do not have the tag "eco-friendly".
 db.products.find({
   tags:{
     $nin:["eco-friendly"]
   }
-},{productName:1,_id:0}).count()
+},{productName:1,_id:0}) 
 
 //Question-27: Find products with ratings that have exactly 2 entries
 db.products.find({
   ratings:{
     $size: 2
   }
-},{productName:1, ratings:1, _id:0}).count()
+},{productName:1, ratings:1, _id:0}) 
 
 //Question-28: Find products with a "discountedPrice" field present and sort them by discountedPrice in ascending order.
 db.products.find({
   discountedPrice:{
     $exists: true
   }
-},{productName:1, discountedPrice:1, _id:0}).sort({discountedPrice:1}).count()
+},{productName:1, discountedPrice:1, _id:0}).sort({discountedPrice:1}) 
 
 //Question-29: Find products whose dimensions.width is greater than 20 but less than 50.
 db.products.find({
@@ -408,24 +408,24 @@ db.products.find({
     $gt:20,
     $lt:50
   }
-},{productName:1, "dimensions.width":1, _id:0}).count()
+},{productName:1, "dimensions.width":1, _id:0}) 
 
 //Question-30: Find products whose ratings have at least one review from userId "63b21d49c254d7e8456789ad"
 db.products.find({
   "ratings.userId":"63b21d49c254d7e8456789ad"
-},{productName:1, _id:0}).count()
+},{productName:1, _id:0}) 
 
 //Question-31: Find the first product whose tags array contains exactly 3 elements, sorted by productName in ascending order.
 db.products.find({
   tags:{
     $size:3
   }
-},{productName:1, tags:1}).sort({productName:1}).limit(1).count()
+},{productName:1, tags:1}).sort({productName:1}).limit(1) 
 
 //Question-32: Find products where the specs.processor contains the word "Intel".
 db.products.find({
   "specs.processor":/Intel/
-},{productName:1,"specs.processor":1,_id:0}).count()
+},{productName:1,"specs.processor":1,_id:0}) 
 
 //Question-33: Find products with inventory.location where at least one store has "quantity" between 50 and 150, inclusive.
 db.products.find({
@@ -437,7 +437,7 @@ db.products.find({
     }
     }
   }
-},{productName:1, "inventory.location.quantity":1,_id:0}).count()
+},{productName:1, "inventory.location.quantity":1,_id:0}) 
 
 //Question-34: Find products where the dimensions.height is greater than both dimensions.width and dimensions.depth
 db.products.find({
@@ -447,12 +447,12 @@ db.products.find({
       {$max:["$dimensions.width","$dimensions.depth"]}
     ]
     }
-}).count()
+}) 
 
 //Question-35:Find the top 3 most expensive products based on price, but only return productName and price.
 db.products.find({},{
   productName:1, price:1, _id:0
-}).sort({price:-1}).limit(3).count()
+}).sort({price:-1}).limit(3) 
 
 //Question-36: Find products with a location.coordinates longitude less than -100 (Western Hemisphere) and sort them by latitude in ascending order.
 db.products.find({
@@ -460,7 +460,7 @@ db.products.find({
     $lt:-100
   }
   
-},{productName:1, "location.coordinates":1,_id:0}).sort({"location.coordinates.1":1}).count()
+},{productName:1, "location.coordinates":1,_id:0}).sort({"location.coordinates.1":1}) 
 
 //Question-37: Find products where the total inventory quantity across all locations is greater than 200.
 db.products.find({
@@ -472,12 +472,12 @@ db.products.find({
       ]}
     ]
   }
-},{productName:1, "inventory.location.quantity":1, _id:0 }).count()
+},{productName:1, "inventory.location.quantity":1, _id:0 }) 
 
 //Question-38: Find products with at least one rating comment that starts with the letter "E".
 db.products.find({
   "ratings.comment":/^E/
-},{productName:1, "ratings.comment":1, _id:0 }).count()
+},{productName:1, "ratings.comment":1, _id:0 }) 
 
 //Question-39: Find products where the inventory quantity for any store is greater than 100 but less than 200
 db.products.find({
@@ -485,7 +485,7 @@ db.products.find({
     $gt:100,
     $lt:200
   }
-},{"inventory.location.quantity":1, productName:1, _id:0}).count()
+},{"inventory.location.quantity":1, productName:1, _id:0}) 
 
 //Question-40: Find products that have more tags than ratings.
 db.products.find({
@@ -502,7 +502,7 @@ db.products.find({
   tags:{
     $all:["high-performance", "portable"]
   }
-},{productName:1, tags:1,_id:0}).count()
+},{productName:1, tags:1,_id:0}) 
 
 //Question-42: Find products where the discountedPrice is more than 20% lower than the original price.
 db.products.find({
@@ -512,7 +512,7 @@ db.products.find({
       {$multiply:["$price",0.8]}
     ]
   }
-},{productName:1, price:1, discountedPrice:1, _id:0}).count()
+},{productName:1, price:1, discountedPrice:1, _id:0}) 
 
 //Question-43: Find products where the location is within 100 km of a point (-122.4194, 37.7749)
 db.products.find({
@@ -520,7 +520,7 @@ db.products.find({
     $lt:["$location.coordinates.0",-122.4194],
     $lt:["$location.coordinates.0",37.7749],
   }
-},{productName:1, _id:0 }).count()
+},{productName:1, _id:0 }) 
 
 //Question:-44: Find products where the total inventory across all locations is greater than 200.
 db.products.find({
@@ -530,17 +530,17 @@ db.products.find({
       200
     ]
   }
-},{productName:1, _id:0}).count()
+},{productName:1, _id:0}) 
 
 //Question-45: Find the second most recently released product
 db.products.find({},{
   releaseDate:1, productName:1
-}).sort({releaseDate:-1}).skip(1).limit(1).count()
+}).sort({releaseDate:-1}).skip(1).limit(1) 
 
 //Question-46: Find products where at least one feature in specs.features contains the word "LED".
 db.products.find({
   "specs.features":/LED/
-},{productName:1, specs:1, _id:0 }).count()
+},{productName:1, specs:1, _id:0 }) 
 
 //Question-47: Find products with a total rating score of at least 5 across all ratings
 db.products.find({
@@ -550,7 +550,7 @@ db.products.find({
       5
     ]
   }
-},{productName:1, ratings:1, _id:0}).count()
+},{productName:1, ratings:1, _id:0}) 
 
 //Question-48: Find products with an inventory store named "Online Store" that has a quantity of more than 50
 db.products.find({
@@ -558,7 +558,7 @@ db.products.find({
   "inventory.location.quantity":{
     $gt:50
   },
-},{productName:1, inventory:1, _id:0}).count()
+},{productName:1, inventory:1, _id:0}) 
 
 //Question-49: Find products that are both isBestSeller and isFeatured, but exclude those with no ratings
 db.products.find({
@@ -570,13 +570,13 @@ db.products.find({
       0
     ]
   }
-},{productName:1, isBestSeller: 1, isFeatured: 1, _id:0}).count()
+},{productName:1, isBestSeller: 1, isFeatured: 1, _id:0}) 
 
 db.products.find({
   isBestSeller: true,
   isFeatured: true,
   ratings: { $exists: true, $not: { $size: 0 } }
-}, { productName: 1, isBestSeller: 1, isFeatured: 1, _id: 0 }).count()
+}, { productName: 1, isBestSeller: 1, isFeatured: 1, _id: 0 }) 
 
 //Question-50: Find products where the maximum inventory quantity across all locations exceeds 150
 db.products.find({
@@ -591,7 +591,7 @@ db.products.find({
 //Question-51: Find the top 3 tallest products in dimentions
 db.products.find({},{
   productName:1,_id:0
-}).sort({"dimensions.height":-1}).limit(3).count()
+}).sort({"dimensions.height":-1}).limit(3) 
 
 //Question-52: Find products where any of the tags contains "eco" as a substring, ignoring case.
 db.products.find({
@@ -617,7 +617,7 @@ db.products.find({
   }
 },{productName:1,price:1,_id:0}).sort({
   price:1
-}).count()
+}) 
 
 //Question-54: Find products where the releaseDate is in june of 2023, and sort them by price in ascending order
 db.products.find({
@@ -627,14 +627,14 @@ db.products.find({
   }
 },{productName:1,price:1,_id:0}).sort({
   price:1
-}).count()
+}) 
 
 //Question-55: Find products that are available, have a warranty of "2 years", and have a price greater than 500
 db.products.find({
   available:true,
   warranty:"2 years",
   price:{$gt:500}
-},{productName:1, _id:0}).count()
+},{productName:1, _id:0}) 
 
 //Question-56: Find products where the ratings field has more than one review, but the average score of all reviews is greater than or equal to 4.
 db.products.find({
@@ -647,7 +647,7 @@ db.products.find({
   $expr: {
     $gte: [{ $avg: "$ratings.score" }, 4]
   }
-}, { productName: 1, ratings: 1, _id: 0 }).count()
+}, { productName: 1, ratings: 1, _id: 0 }) 
 
 //Question-57: Find products whose price is between 200 and 1000, but not equal to 450.
 db.products.find({
@@ -656,13 +656,13 @@ db.products.find({
     $gt:200,
     $ne:450
   }
-},{productName:1,price:1}).count()
+},{productName:1,price:1}) 
 
 //Question-58: Find products that have both isFeatured and isBestSeller set to true, and return only their productName and tags.
 db.products.find({
   isFeatured: true,
   isBestSeller: true
-}, { productName: 1, tags: 1, _id: 0 }).count()
+}, { productName: 1, tags: 1, _id: 0 }) 
 
 
 //Question-59: Find products with at least one relatedProduct that has a price greater than 2000.
@@ -674,7 +674,7 @@ db.products.find({
     ]
   },
   price:{$gt:2000}
-},{productName:1,relatedProducts:1, price:1, _id:0}).count()
+},{productName:1,relatedProducts:1, price:1, _id:0}) 
 
 //Question-60: Find products whose dimensions field contains both width and depth greater than 20
 db.products.find({
@@ -684,13 +684,13 @@ db.products.find({
   "dimensions.depth":{
     $gt:20
   }
-}).count()
+}) 
 
 //Question-61: Find products where the ratings array contains a review with a score of 5, and that is by the user with userId "63b21d49c254d7e8456789ab".
 db.products.find({
   "ratings.score":5,
   "ratings.userId":"63b21d49c254d7e8456789ab"
-},{productName:1, _id:0}).count()
+},{productName:1, _id:0}) 
 
 //Question-62: Find products where the ratings field contains a review with a score greater than or equal to 4.5, and sort by the reviewDate in descending order.
 db.products.find({
@@ -698,7 +698,7 @@ db.products.find({
     $gte: 4.5 
     
   }
-},{productName:1, _id:0}).sort({ "ratings.reviewDate": -1 }).count()
+},{productName:1, _id:0}).sort({ "ratings.reviewDate": -1 }) 
 
 //Question-63: Find products where price is less than 1000, and they have at least one inventory.location with quantity greater than 50.
 db.products.find({
