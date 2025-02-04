a5372435537a21d1991fe0ab8af01b515425a928
Rahul Panchal <Rahul.Panchal@upforce.tech>
2025-02-04 12:54:42 +0530

    add folders

---
+++ Aggregate_Pipeline/LookupGroupProject.js
@@ -0,0 +1,335 @@
+// Basic Lookup:
+
+// Join the orders collection with the customers collection based on the customerId field. Include customer details in the result.
+// Nested Lookup:
+
+// Join the orders collection with the customers collection first, then join the result with the products collection. Include order details, customer details, and product details.
+// Lookup with Multiple Fields:
+
+// Join the orders collection with the products collection based on both productId and categoryId. Include product details in the result for each order.
+// Lookup with Pipeline:
+
+// Join the orders collection with the products collection using a pipeline. Filter products based on a certain condition, such as price range or category.
+// Lookup with Date Range:
+
+// Join the orders collection with the products collection and filter orders based on a specific date range. Include product details in the result.
+// Lookup with Sorting:
+
+// Join the orders collection with the products collection and sort the result based on a certain field, such as product price or order date.
+// Lookup with Limit and Skip:
+
+// Join the orders collection with the products collection and limit the number of results returned. Optionally, skip a certain number of documents before returning results.
+// Lookup with Custom Field Names:
+
+// Join the orders collection with the products collection and specify custom field names for the output fields, such as renaming productId to product_id or productName to product_name.
+
+// Inserting more products
+db.products.insertMany([
+    {
+        "id": 124,
+        "productName": "Smartphone",
+        "price": 800,
+        "category": "Electronics"
+    },
+    {
+        "id": 125,
+        "productName": "Headphones",
+        "price": 100,
+        "category": "Electronics"
+    },
+    {
+        "id": 126,
+        "productName": "Coffee Maker",
+        "price": 50,
+        "category": "Appliances"
+    },
+    {
+        "id": 127,
+        "productName": "Running Shoes",
+        "price": 120,
+        "category": "Sports"
+    }
+]);
+
+// Inserting more orders
+db.orders.insertMany([
+    {
+        "id": 13,
+        "customerId": "C1002",
+        "items": [
+            { "productId": 124, "quantity": 1 },
+            { "productId": 127, "quantity": 2 }
+        ],
+        "orderDate": ISODate("2024-01-05"),
+        "status": "delivered"
+    },
+    {
+        "id": 14,
+        "customerId": "C1003",
+        "items": [
+            { "productId": 125, "quantity": 3 },
+            { "productId": 127, "quantity": 3 }
+        ],
+        "orderDate": ISODate("2024-01-10"),
+        "status": "shipped"
+    },
+    {
+        "id": 15,
+        "customerId": "C1001",
+        "items": [
+            { "productId": 126, "quantity": 1 }
+        ],
+        "orderDate": ISODate("2024-01-15"),
+        "status": "pending"
+    }
+]);
+
+// Inserting more Customens 
+db.customers.insertMany([
+    {
+        "customerId": "C1001",
+        "name": "John Doe",
+        "email": "john@example.com",
+        "address": "123 Main St"
+    },
+    {
+        "customerId": "C1002",
+        "name": "Jane Smith",
+        "email": "jane@example.com",
+        "address": "456 Elm St"
+    },
+    {
+        "customerId": "C1003",
+        "name": "Alice Johnson",
+        "email": "alice@example.com",
+        "address": "789 Oak St"
+    }
+]);
+
+// Basic Lookup:
+// Question: Perform a basic $lookup operation to join the orders collection with the products collection. Include the product details in the result for each order.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "products"
+        }
+    }
+])
+//add products in items array
+db.orders.aggregate([
+    { $unwind: "$items" },
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "pd"
+        }
+    },
+    { $unwind: "$pd" },
+    {
+        $group: {
+            _id: "$_id",
+            "customerId": { $first: "$customerId" },
+            "orderDate": { $first: "$orderDate" },
+            "status": { $first: "$status" },
+            item: {
+                $push: {
+                    quantity: "$items.quantity",
+                    pd: "$pd"
+                }
+            }
+        }
+    }
+])
+// Question: Join the orders collection with the customers collection first, and then join the result with the products collection. Include order details, customer details, and product details in the final output.
+db.orders.aggregate([
+    { $unwind: "$items" },
+    {
+        $lookup: {
+            from: "customers",
+            localField: "customerId",
+            foreignField: "customerId",
+            as: "cus_Det"
+        }
+    },
+    { $unwind: "$cus_Det" },
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "pr_Det"
+        }
+    },
+    { $unwind: "$pr_Det" },
+    {
+        $group: {
+            _id: "$id",
+            customerId: { $first: "$customerId" },
+            status: { $first: "$status" },
+            items: {
+                $push: {
+                    quantity: "$items.quantity",
+                    prname: "$pr_Det.productName",
+                }
+            },
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and filter products with a price greater than $500. Include only the matching products in the result.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "products"
+        }
+    },
+    {
+        $addFields: {
+            products: {
+                $filter: {
+                    input: "$products",
+                    as: "pr",
+                    cond: { $gt: ["$$pr.price", 500] }
+                }
+            }
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and group orders by product category. Include the count of orders and product details for each category.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "ordered_products"
+        }
+    },
+    {
+        $unwind: "$ordered_products"
+    },
+    {
+        $group: {
+            _id: "$ordered_products.category",
+            total_orders: { $sum: 1 },
+            products: { $push: "$ordered_products" }
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and sort the result based on product price in descending order.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "ordered_products"
+        }
+    },
+    { $unwind: "$ordered_products" },
+    { $sort: { "ordered_products.price": -1 } },
+]);
+// Question: Join the orders collection with the products collection and limit the result to include only the first 5 products.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "ordered_products"
+        }
+    },
+    { $unwind: "$ordered_products" },
+    { $limit: 5 }
+]);
+// Question: Join the orders collection with the products collection and calculate the total revenue for each order.
+db.orders.aggregate([
+    { $unwind: "$items" },
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "pd"
+        }
+    },
+    { $unwind: "$pd" },
+    {
+        $group: {
+            _id: "$id",
+            customerId: { $first: "$customerId" },
+            status: { $first: "$status" },
+            items: {
+                $push: {
+                    quantity: "$items.quantity",
+                    productName: "$pd.productName",
+                    productID: "$pd.id",
+                    totalAmount: { $multiply: ["$items.quantity", "$pd.price"] }
+                }
+            }
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and project only the necessary fields from the product details.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "ordered_products"
+        }
+    },
+    { $unwind: "$ordered_products" },
+    {
+        $project: {
+            productName: "$ordered_products.productName",
+            price: "$ordered_products.price"
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and rename the output fields for product details to item_name and item_price.
+db.orders.aggregate([
+    {
+        $lookup: {
+            from: "products",
+            localField: "items.productId",
+            foreignField: "id",
+            as: "ordered_products"
+        }
+    },
+    {
+        $unwind: "$ordered_products"
+    },
+    {
+        $project: {
+            item_name: "$ordered_products.productName",
+            item_price: "$ordered_products.price"
+        }
+    }
+]);
+// Question: Join the orders collection with the products collection and include only the products that belong to a specific category.
+db.orders.aggregate([
+    { $lookup : {
+        from:"products",
+        let : { orderItems : "$items" },
+        pipeline : [
+             { $match: { 
+                  $expr: { 
+                    $and: [
+                      { $in: ["$id", "$$orderItems.productId"] },
+                      { $eq: ["$category", "Electronics"] }
+                    ] 
+                  } 
+        } }
+        ],
+        as : "ordered_products"
+    } },
+]);
---
+++ Aggregate_Pipeline/Project.js
@@ -0,0 +1,81 @@
+// Question: Project only the product name and price fields for all documents.
+db.products.aggregate([
+    {
+        $project: { productName: 1, price: 1, _id: 0 }
+    }
+])
+// Question: Exclude the _id field and include all other fields for documents.
+db.products.aggregate([
+    { $project: { _id: 0 } }
+]);
+
+//Question: Exclude the _id field and include all other fields for documents.
+db.products.aggregate([
+    {
+        $project: { productName: 1, salesPrice: { $multiply: ["$price", 0.10] } }
+    }
+])
+// Question: Project the product name and a new field priceAfterTax which includes a 15% tax.
+db.products.aggregate([
+    {
+        $project: { _id: 0, productName: 1, tex: { $multiply: ["$price", 1.16] } }
+    }
+])
+// Question: Display the category and a new field isExpensive that is true if the price is greater than 1000.
+db.products.aggregate([
+    {
+        $project: { _id: 0, category: 1, isExpensive: { $gt: ["$price", 1000] } }
+    }
+])
+// Question: Compute and display a new field discountedPrice which is 20% less than the original price.
+db.products.aggregate([
+    {
+        $project: { _id: 0, productName: 1, discount: { $multiply: ["$price", 0.80] } }
+    }
+])
+// Question: Show product name and a new array tagsCount that contains the count of tags each product has.
+db.products.aggregate([
+    {
+        $project: { _id: 0, productName: 1, tagsCount: { $size: "$tags" } }
+    }
+])
+// Question: Include all original fields but add a new field inventoryValue which is the product of price and a field stock if it exists.
+db.products.aggregate([
+    {
+        $project: {
+            productName: 1,
+            invertoryValue: { $ifNull: [{ $multiply: ["$price", "$stock"] }, "N/A"] }
+        }
+    }
+]);
+// Question: Display product name and a new field ratingAverage that is the average of the scores from the ratings array.
+db.products.aggregate([
+    {
+        $project: {
+            productName: 1,
+            ratingAverage: { $avg: "$ratings.score" }
+        }
+    },
+]);
+
+
+// Question: Show all products with their name, price, and 
+// a new field priceRange indicating if they are 'cheap', 
+// 'moderate', or 'expensive' based on the price.
+db.products.aggregate([
+    {
+        $project: {
+            productName: 1,
+            pricing: {
+                $switch: {
+                    branches: [
+                        { case: { $gt: ["$price", 1000] }, then: "exp" },
+                        { case: { $lt: ["$price", 1000] }, then: "cheap" },
+                    ],
+                    default: "moderate"
+                }
+            }
+        }
+    },
+]);
+
